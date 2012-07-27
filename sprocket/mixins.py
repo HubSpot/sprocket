46947d6fc2bbfae31e2e10cd3e1d7760bd3f1a92
pfitzsimmons <pfitzsimmons@hubspot.com>
2012-07-27 18:56:53 -0400

    adding mixins logic

--- sprocket/mixins.py
+++ sprocket/mixins.py
@@ -1,2 +1,25 @@
 
 
+
+
+
+class BaseMixin(object):
+    def __init__(self, api_resource):
+        self.api = api_resource
+        self.event_handler_by_event_name = self._construct_events_dict()
+        
+    def _construct_events_dict(self):
+        handlers_by_name = {}
+        for attr_name in dir(self):
+            if attr_name.startswith('on_'):
+                event_name = attr_name[3:]
+                handlers_by_name[event_name] = getattr(self, attr_name)
+        return handlers_by_name
+
+    def get_handler_for_event(self, event_name):
+        return self.event_handler_by_event_name.get(event_name, None)
+                
+    def mixin_urls(self):
+        return []
+
+
